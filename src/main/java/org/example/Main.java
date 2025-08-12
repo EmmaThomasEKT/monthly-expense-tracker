66b2286b76f775cb2bbe9c4515b21151d173b14b
EmmaThomasEKT <ethomas210200@gmail.com>
2025-08-12 20:17:39 +1000

    Completed a basic menu function

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,31 @@
 package org.example;
+import java.util.*;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println();
+        System.out.println("""
+                1. Set Monthly Budget (Optional)
+                2. Define expenses categories
+                3. Quit
+                """);
+        int input = scanner.nextInt();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        switch (input) {
+            case 1:
+                MainMenu.monthlyBudget(scanner);
+                break;
+            case 2:
+                MainMenu.defineExpenses(scanner);
+                break;
+            case 3:
+                System.out.println("Thank you for using Monthly Expense Tracker!");
+                System.exit(0);
+            default:
+                System.out.println("Invalid input. Please choose an option (1/2/3)");
         }
+
+        scanner.close();
     }
 }
---
+++ src/main/java/org/example/MainMenu.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class MainMenu {
+
+    public static void monthlyBudget(Scanner scanner) {
+
+    }
+
+    public static void defineExpenses(Scanner scanner) {
+
+    }
+
+}
